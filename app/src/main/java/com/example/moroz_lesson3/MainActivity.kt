ee3d1e42c0db6b24959363cfdfc054e527a87e03
Aleksandr Moroz <morozaleksandriv@gmail.com>
2023-02-15 15:02:33 +0300

    moroz_lesson_3_2 finished placing elements on the activity_main.xml, added button listeners

--- app/src/main/java/com/example/moroz_lesson3/MainActivity.kt
+++ app/src/main/java/com/example/moroz_lesson3/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.moroz_lesson3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.VIEW_MODEL_STORE_OWNER_KEY
+import com.example.moroz_lesson3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        actionAfterClickInContacts()
+        actionAfterClickInVacancies()
+        actionAfterClickInGallery()
     }
+
+    private fun actionAfterClickInContacts() {
+        with(binding) {
+            contacts.setOnClickListener {
+                group.visibility = View.GONE
+                progress.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun actionAfterClickInVacancies() {
+        with(binding) {
+            vacancies.setOnClickListener {
+                group.visibility = View.GONE
+                progress.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun actionAfterClickInGallery() {
+        with(binding) {
+            gallery.setOnClickListener {
+                group.visibility = View.GONE
+                progress.visibility = View.VISIBLE
+            }
+        }
+    }
+
 }
