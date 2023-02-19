9b685bd689dec71c8abf5f91a2d5ad023c2040a2
Aleksandr Moroz <morozaleksandriv@gmail.com>
2023-02-19 20:19:33 +0300

    moroz_lesson_4_2 created aston_button.xml and AstonButtonView.kt

---
+++ app/src/main/java/com/example/moroz_lesson3/AstonButtonView.kt
@@ -0,0 +1,16 @@
+package com.example.moroz_lesson3
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class AstonButtonView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defResAttrs: Int = 0,
+    defResStyles: Int = 0,
+) : ConstraintLayout(context, attrs, defResAttrs, defResStyles) {
+
+
+
+}
