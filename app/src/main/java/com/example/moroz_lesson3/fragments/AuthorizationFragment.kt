9e1a1fa6933b1620fc5489010993c527c4787fa4
Aleksandr Moroz <morozaleksandriv@gmail.com>
2023-02-20 22:48:07 +0300

    moroz_lesson_5_2 created layouts: authorization, main screen, vacancies

---
+++ app/src/main/java/com/example/moroz_lesson3/fragments/AuthorizationFragment.kt
@@ -0,0 +1,29 @@
+package com.example.moroz_lesson3.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.moroz_lesson3.R
+import com.example.moroz_lesson3.databinding.FragmentAuthorizationBinding
+
+class AuthorizationFragment : Fragment() {
+
+    private var _binding: FragmentAuthorizationBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentAuthorizationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
---
+++ app/src/main/java/com/example/moroz_lesson3/fragments/VacanciesFragment.kt
@@ -0,0 +1,30 @@
+package com.example.moroz_lesson3.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.moroz_lesson3.R
+import com.example.moroz_lesson3.databinding.FragmentVacanciesBinding
+
+class VacanciesFragment : Fragment() {
+
+    private var _binding: FragmentVacanciesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentVacanciesBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
