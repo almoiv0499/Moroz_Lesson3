dc84bf731ac03d3266e82b7ec23caafa0e9d6e50
Aleksandr Moroz <morozaleksandriv@gmail.com>
2023-02-22 21:48:51 +0300

    moroz_lesson_5_2 added OfficesDetailsViewModel.kt

---
+++ app/src/main/java/com/example/moroz_lesson3/fragments/details/OfficesDetailsViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.moroz_lesson3.fragments.details
+
+import com.example.moroz_lesson3.fragments.base.BaseViewModel
+
+class OfficesDetailsViewModel : BaseViewModel() {
+
+    fun navigateToOfficesFragment() {
+        navigateToBack()
+    }
+
+}
