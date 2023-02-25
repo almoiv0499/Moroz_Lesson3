3c8baf78a24b91cee5aea6c5e7e77c470c31923a
Aleksandr Moroz <morozaleksandriv@gmail.com>
2023-02-25 20:34:15 +0300

    moroz_lesson_6 fixed attrs names

--- app/src/main/java/com/example/moroz_lesson3/recyclerview/vacancies/VacanciesViewHolder.kt
+++ app/src/main/java/com/example/moroz_lesson3/recyclerview/vacancies/VacanciesViewHolder.kt
@@ -4,19 +4,20 @@ import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bumptech.glide.Glide
 import com.example.moroz_lesson3.R
 import com.example.moroz_lesson3.databinding.VacancyRvItemBinding
+
 import com.example.moroz_lesson3.model.Vacancy
 
 class VacanciesViewHolder(private val binding: VacancyRvItemBinding) : ViewHolder(binding.root) {
 
     fun bind(vacancy: Vacancy) {
         with(binding) {
-            titleCustomView.text = vacancy.title
-            subtitleCustomView.text = vacancy.subtitle
+            vacancyTitle.text = vacancy.title
+            vacancySubtitle.text = vacancy.subtitle
             Glide.with(binding.root)
                 .load(vacancy.image)
                 .fitCenter()
                 .placeholder(R.drawable.logo)
-                .into(binding.logoCustomView)
+                .into(binding.vacancyLogo)
         }
     }
 
