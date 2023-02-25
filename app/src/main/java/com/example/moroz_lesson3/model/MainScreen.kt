47662898997199492dd5ff0325c65c43e6f6d3e8
Aleksandr Moroz <morozaleksandriv@gmail.com>
2023-02-25 20:48:31 +0300

    moroz_lesson_6 added const

--- app/src/main/java/com/example/moroz_lesson3/model/MainScreen.kt
+++ app/src/main/java/com/example/moroz_lesson3/model/MainScreen.kt
@@ -5,9 +5,13 @@ import com.example.moroz_lesson3.R
 data class MainScreen(
     val image: Int = R.drawable.title_image,
     val logo: Int = R.drawable.logo,
-    val title: String = "",
-    val subtitle: String = "",
-    val aboutUs: String = "",
-    val aboutUsDescription: String = "",
+    val title: String = DEFAULT_VALUE,
+    val subtitle: String = DEFAULT_VALUE,
+    val aboutUs: String = DEFAULT_VALUE,
+    val aboutUsDescription: String = DEFAULT_VALUE,
     val viewType: Int = 0
-)
+) {
+    companion object {
+        private const val DEFAULT_VALUE = ""
+    }
+}
